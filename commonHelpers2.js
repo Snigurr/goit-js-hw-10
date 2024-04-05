46d7e1ebdd9ab07c5f48fb425e15d720ecfb5aff
Snigurr <142939158+Snigurr@users.noreply.github.com>
2024-04-05 23:02:41 +0000

    Deploying to gh-pages from @ Snigurr/goit-js-hw-10@2a17f65dcb8357621e95b0fd9315c239c0dd28a7 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as o}from"./assets/vendor-cd4b7665.js";o.show({title:"Hey",message:"What would you like to add?"});
+//# sourceMappingURL=commonHelpers2.js.map
